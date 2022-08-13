f1bcaf6f864810051b471093615518c76f229371
awpgikxdigj <etantse06@gmail.com>
2022-08-13 23:25:10 +0800

    - got someone who knows what they're doing get darkmode to toggle on button press - html is now in a separate file - Schedule for 16th August added

--- main.py
+++ main.py
@@ -1,68 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import request
+from flask import jsonify
 import os
 import time
 import datetime
-current = "0"
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    name = request.args.get("name", "")
-    date = request.args.get("date", "")
-    mode = request.args.get("mode", "0")
+    req = request.full_path
+    print(req)
+    name = ""
+    date = ""
+    mode = "light"
+    if "&" in req:
+        req = req.split("&")
+        name = req[0].split("=")[1]
+        date = req[1].split("=")[1].replace("+", " ")
+        mode = req[2].split("=")[1]
     out = ""
     if name and date:
         out = schedule(name, date)
-    return ("""<style>
-    table{width:fit-content;border-collapse: collapse;margin:auto;margin-top: 10px;}
-    td,th{margin-left: auto;
-  margin-right: auto;}
-    td{height:5rem;border: 1px solid white; padding:10px; margin: 10px;}
-    th{height:5rem;border: 3px solid white; padding:10px;font-size:1.2rem;font-weight:bold;}""" + str(change(mode)) + 
-    """</style>
-<span style="font-size: 18px;">
-<h1><p style="font-family: courier">SST MTR Schedule Fetcher</p></h1>
-<p style="font-family: courier">Enter your name to get a list of all matching entries.
-        For exact matches, enter as seen in class register.</p>""" + "<br>" +
-        """<form action="" method="get">
-                <input type="text" name="name"
-                style="height:60px; width:500px; font-size:20px;" type="numeric">
-                <select name="date" id="date"
-                style="height:60px; width:200px; font-size:20px;" type="numeric">
-                  <option value="Today">Today</option>
-                  <option value="11 Aug">11 Aug</option>
-                  <option value="12 Aug">12 Aug</option>
-                </select>
-                <input type="submit" value="Enter" style="height:60px; width:200px; font-size:20px;"><br><br>
-                <input type="checkbox" id="mode" name="mode" value = "1" style="height:20px; width:20px">
-                <label for="mode"><p style="font-family: courier">Toggle Dark mode</p></label>
-              </form><p style="font-family: courier">
-              """ + str(out) +
-            """</p><p style="font-family: courier"><br>
-Only schedules for 11 Aug and 12 Aug are implemented.<br>
-github link: https://github.com/awpgikxdigj/MTRSchedule<br><br>
-Made by Ethan Tse Chun Lam, S407 (objectively better computing class)<br>
-Dark theme by Jerick Seng, S401 (L take)</p>""")
-
-def change(mode):
-    global current
-    if mode == "0":
-        return darkmode(current)
-    elif mode == "1":
-        if current == "0":
-            current = "1"
-        elif current == "1":
-            current = "0"
-        return darkmode(current)
-
-def darkmode(mode):
-    if mode == "0":
-        return ""
-    elif mode == "1":
-        return """body{background: #000000; color:#ffffff}
-    input,select{background:#444444;color:#ffffff}"""
+    print(out)
+    return render_template("index.html", out = out, mode = mode)
 
 def schedule(name, date):
     directory = os.getcwd() + "/data"
@@ -75,14 +36,14 @@ def schedule(name, date):
     try:
         if date == "Today":
             temp = int(str(datetime.datetime.fromtimestamp(int(time.time()+28800))).split()[0].split("-")[2])
-            if temp not in [11, 12]:
-                message = "No schedule for Aug " + temp
+            if temp not in [11, 12, 15, 16]:
+                message = [["No schedule for Aug " + str(temp)]]
                 return message
             else:
                 date = "/" + str(int(temp)) + " Aug"
         else:
             date = "/" + date
-        name = name.lower()
+        name = name.lower().replace("+", " ")
         picknames = []
         for b in fullnames:
             if name in b:
@@ -95,43 +56,47 @@ def schedule(name, date):
         for c in picknames:
             intout = []
             for a in times:
-                flag = len(intout)
                 f = open(directory + "/dates" + date + "/" + a, "r")
                 classes = f.readlines()
                 for b in classes:
                     details = b.split("█")
+                    print(details)
                     names = details[2].strip().split("|")
                     if c in names:
                         intout.append([details[0], details[1]])
                         break
+                else:
+                    intout.append(["no class found", "sorry"])
             out.append(intout)
-        message = ""
+        schedules = []
         if len(out) == 0:
-            message += "schedule not found"
+            return [["Schedule not found"]]
         else:
             for b in range(len(out)):
+                current = []
+                line = ""
                 if len(out[b]) == 0:
                     pass
                 else:
-                    message += "<br><b>"
                     fragname = picknames[b].split()
                     for x in fragname:
-                        message += x.capitalize()
+                        line += x.capitalize()
                         if x == fragname[-1]:
                             pass
                         else:
-                            message += " "
-                    message += "'s schedule on " + date[1:] + "</b><br><br>"
+                            line += " "
+                    line += "'s schedule on " + date[1:]
+                    current.append(line)
                     for a in range(len(out[b])):
-                        message += str(times[a][:-4]) + ": " + str(out[b][a][0])
+                        line = ""
+                        line += str(times[a][:-4]) + ": " + str(out[b][a][0])
                         if out[b][a][1] != " ":
-                            message += " (" + out[b][a][1] + ")<br>"
-                        else:
-                            message += "<br>"
-                        message += "<br>"
-        return message
+                            line += " (" + out[b][a][1] + ")"
+                        current.append(line)
+                schedules.append(current)
+        return schedules
     except ValueError:
-        return "schedule not found"
+        return [["Schedule not found"]]
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
